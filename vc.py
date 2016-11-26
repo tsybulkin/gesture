af0eae5018282980b075deb68785e130624c7179
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2016-11-26 20:04:32 +0200

    wip

--- vc.py
+++ vc.py
@@ -1,5 +1,7 @@
 import cv2                              
 import numpy as np  
+import face
+
 
 def octav(N,img): 
 	if N <= 0: return img
@@ -8,15 +10,35 @@ def octav(N,img):
 
 def demo():
 	cap = cv2.VideoCapture(0)
+	ret,img = cap.read() 
+	img = octav(2,img)
+	prvs = cv2.cvtColor(np.fliplr(img),cv2.COLOR_BGR2GRAY)
+	diff = np.zeros_like(prvs)
+		
 	# define range of Ycc
-	lower_skin = np.array([80,130,80])
-	upper_skin = np.array([200,175,125])
-
+	lower_skin = np.array([80, 130, 80])
+	upper_skin = np.array([200, 175, 125])
+	i = 0
+	face_coords = np.zeros(2)
 
 	while( cap.isOpened() ) :
 		ret,img = cap.read() 
 		img = octav(2,img) 
 		img = np.fliplr(img)
+		next = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+		diff = abs(next - prvs)
+
+		#flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+		#mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+
+		#diff[...,0] = ang*180/np.pi/2
+		#diff[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+
+		#bgr = cv2.cvtColor(diff,cv2.COLOR_GRAY2BGR)
+		cv2.imshow('frame2',diff)
+
+		if i%30 == 0: face_coords = face.get_coords(img)		
 
 		#hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 		ycc = cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
@@ -24,10 +46,8 @@ def demo():
 		mask = cv2.inRange(ycc, lower_skin, upper_skin)
 		masked = cv2.bitwise_and(img,img, mask=mask)
 
-		#blur = cv2.GaussianBlur(gray,(5,5),0)
-		#ret,thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-		cv2.imshow('input',masked)  
+		
+		cv2.imshow('input', masked)  
 
 		k = cv2.waitKey(10)
 		if k == 27:
