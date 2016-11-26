e16338be6508d63c10d13e1d32dea579e97e5dd7
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2016-11-26 16:22:14 +0200

    from skin detection module main loop extracted

--- vc.py
+++ vc.py
@@ -1,27 +1,40 @@
 import cv2                              
 import numpy as np  
 
-cap = cv2.VideoCapture(0)                
-while( cap.isOpened() ) :
-	ret,img = cap.read()   
-	img = np.fliplr(img)
+def octav(N,img): 
+	if N <= 0: return img
+	return octav(N-1,cv2.pyrDown(img))
 
-	#hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-	ycc = cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
 
+def demo():
+	cap = cv2.VideoCapture(0)
 	# define range of Ycc
-	lower_skin = np.array([100,135,85])
-	upper_skin = np.array([200,180,135])
+	lower_skin = np.array([80,130,80])
+	upper_skin = np.array([200,175,125])
 
-	# Threshold the HSV image to get only blue colors
-	mask = cv2.inRange(ycc, lower_skin, upper_skin)
-	masked = cv2.bitwise_and(img,img, mask=mask)
 
-	#blur = cv2.GaussianBlur(gray,(5,5),0)
-	#ret,thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+	while( cap.isOpened() ) :
+		ret,img = cap.read() 
+		img = octav(2,img) 
+		img = np.fliplr(img)
 
-	cv2.imshow('input',masked)  
+		#hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+		ycc = cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
 
-	k = cv2.waitKey(10)
-	if k == 27:
-		break
+		mask = cv2.inRange(ycc, lower_skin, upper_skin)
+		masked = cv2.bitwise_and(img,img, mask=mask)
+
+		#blur = cv2.GaussianBlur(gray,(5,5),0)
+		#ret,thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+		cv2.imshow('input',masked)  
+
+		k = cv2.waitKey(10)
+		if k == 27:
+			break
+
+
+if __name__ == '__main__':
+	demo()
+
+
